e41068d93fef0400661c5f5d72a30e6018cd3eaf
Everard de Vree <everarddevree@gmail.com>
2026-01-16 14:51:23 +0100

    fix eslint config, so that ESLint's prettier checking is actually done by the local prettier config file, and not the recommended config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
 // @ts-check
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import prettier from "eslint-plugin-prettier/recommended";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...tseslint.configs.stylistic,
-  {
-    ignores: ["node_modules/", "packages/*/build/", "reports/"],
-  },
-  prettier,
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	...tseslint.configs.stylistic,
+
+	{
+		ignores: ['node_modules/', 'packages/*/build/', 'reports/'],
+	},
+	eslintConfigPrettier,
 ];
